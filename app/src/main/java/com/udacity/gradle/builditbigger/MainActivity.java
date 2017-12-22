f1c495219701eebd693e36af0391ea620d24f8ed
ricardo nakayama <ricardohn@ricardos-MacBook-Air.local>
2017-12-21 22:22:49 -0200

    Adding step 3

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -1,15 +1,12 @@
 package com.udacity.gradle.builditbigger;
 
-import android.content.Intent;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Pair;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Toast;
-
-import com.example.Joker;
-import com.example.jokerandroidlib.JokerActivity;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -44,11 +41,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void tellJoke(View view) {
-        Joker joker = new Joker();
-        Intent intent = new Intent(this, JokerActivity.class);
-        intent.putExtra(JokerActivity.EXTRA_JOKER, joker.getJoke());
-        startActivity(intent);
+        new EndpointsAsyncTask().execute(this);
     }
-
-
 }
